f41d723c7d1379392d12df91f3dc8b16aeb738f4
vijayandika <vijayandika458@gmail.com>
2024-03-07 21:22:25 +0700

    comment

--- src/components/VerifyCodeForm.vue
+++ src/components/VerifyCodeForm.vue
@@ -52,7 +52,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+// import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "../store/modules/auth";
 
