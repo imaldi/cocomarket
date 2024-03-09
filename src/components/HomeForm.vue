95e9e2934f38a9f3492b58337efbc96d60e4fd06
vijayandika <vijayandika458@gmail.com>
2024-03-09 17:21:50 +0700

    icon

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -13,14 +13,9 @@
             remote-show-suffix
             :remote-method="remoteMethod"
             :loading="loading"
-            style="width: 100%;"
+            style="width: 100%"
           >
-            <el-option
-              v-for="item in dataSearch"
-              :key="item.name"
-              :label="item.name"
-              :value="item.value"
-            />
+            <el-option v-for="item in dataSearch" :key="item.name" :label="item.name" :value="item.value" />
             <!-- <template #prefix>
               <img
                 src="../assets/icon/search.svg"
@@ -33,12 +28,7 @@
           </el-select>
           <div class="my-auto ml-2">
             <div class="bg-light rounded-md p-1">
-              <icon
-                icon="clarity:bell-solid-badged"
-                color="#000"
-                width="28"
-                height="28"
-              />
+              <icon icon="clarity:bell-solid-badged" color="#000" width="28" height="28" />
             </div>
           </div>
         </div>
@@ -48,13 +38,8 @@
         <div class="bg-white rounded-xl shadow-lg">
           <div class="mx-4 p-2">
             <div class="flex">
-              <icon
-                icon="streamline:wallet-solid"
-                color="#E68027"
-                width="28"
-                height="28"
-              />
-              <div class="my-auto ml-2 font-bold">CocoPay</div>
+              <iconnative icon="wallet" class="my-auto" color="#E68027" width="18" height="18" />
+              <div class="my-auto ml-1 text-xs font-bold">CocoPay</div>
             </div>
             <div class="flex justify-between text-center">
               <div class="text-3xl font-bold">
@@ -69,30 +54,15 @@
               </div>
               <div class="flex">
                 <div class="mr-0.5">
-                  <icon
-                    icon="ri:qr-scan-line"
-                    color="#000"
-                    width="28"
-                    height="28"
-                  />
+                  <iconnative icon="withdraw" color="#E68027" width="28" height="28" />
                   <div class="text-xs">Pay</div>
                 </div>
                 <div class="mr-0.5">
-                  <icon
-                    icon="iconoir:add-square"
-                    color="#000"
-                    width="28"
-                    height="28"
-                  />
+                  <iconnative icon="outline-plus" color="#E68027" width="28" height="28" />
                   <div class="text-xs">TopUp</div>
                 </div>
                 <div class="mr-0.5">
-                  <icon
-                    icon="iconoir:menu"
-                    color="#000"
-                    width="28"
-                    height="28"
-                  />
+                  <iconnative icon="more" color="#E68027" width="28" height="28" />
                   <div class="text-xs">More</div>
                 </div>
               </div>
@@ -104,20 +74,13 @@
       <div class="mx-8">
         <div class="pt-4 pb-4 font-bold">Menu</div>
         <div class="flex justify-between">
-          <div
-            @click="router.push('/findfreshfood')"
-            class="rounded-2xl p-4 flex"
-            style="background-color: #ffe7b1"
-          >
+          <div @click="router.push('/findfreshfood')" class="rounded-2xl p-4 flex" style="background-color: #ffe7b1">
             <div class="my-auto mr-2">
               <img src="../assets/img/fresh-food.png" alt="fresh-food" />
             </div>
             <div class="my-auto">Fresh Food</div>
           </div>
-          <div
-            class="bg-secondary rounded-2xl p-4 flex"
-            style="background-color: #f6d6d0"
-          >
+          <div class="bg-secondary rounded-2xl p-4 flex" style="background-color: #f6d6d0">
             <div class="my-auto mr-2">
               <img src="../assets/img/cocoling.png" alt="cocoling" />
             </div>
@@ -133,14 +96,8 @@
                 GET 10% for Your <br />
                 Fresh Food
               </div>
-              <div class="mt-2">
-                Enjoy discounts of up 10% on your fresh food today
-              </div>
-              <div
-                class="bg-white rounded-full p-2 pr-6 pl-6 text-black w-1/4 text-center mt-2"
-              >
-                Buy Now
-              </div>
+              <div class="mt-2">Enjoy discounts of up 10% on your fresh food today</div>
+              <div class="bg-white rounded-full p-2 pr-6 pl-6 text-black w-1/4 text-center mt-2">Buy Now</div>
             </div>
           </div>
           <img src="../assets/img/promos.png" width="328" height="228" alt="" />
@@ -148,19 +105,10 @@
 
         <div class="font-bold">Best Deal</div>
         <div style="height: 100%" class="mb-10 pb-8">
-          <div
-            v-if="dataSearch && dataSearch.length > 0"
-            class="grid grid-cols-2 py-5 gap-6"
-          >
+          <div v-if="dataSearch && dataSearch.length > 0" class="grid grid-cols-2 py-5 gap-6">
             <div v-for="(item, index) in dataSearch" :key="index">
               <div class="rounded-xl p-0 mr-6 bg-white">
-                <img
-                  :src="item.image"
-                  width="80"
-                  height="80"
-                  class="w-full justify-center"
-                  alt=""
-                />
+                <img :src="item.image" width="80" height="80" class="w-full justify-center" alt="" />
                 <div>
                   <div>{{ item.name }}</div>
                 </div>
@@ -177,10 +125,7 @@
               </div>
             </div>
           </div>
-          <div
-            v-else-if="dataProduct && dataProduct.length > 0"
-            class="grid grid-cols-2 py-5 gap-6"
-          >
+          <div v-else-if="dataProduct && dataProduct.length > 0" class="grid grid-cols-2 py-5 gap-6">
             <div
               v-for="(item, index) in dataProduct"
               :key="index"
@@ -229,6 +174,7 @@
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import { useHomeStore } from "../store/modules/home";
+import iconnative from "../icon/index.vue";
 
 const router = useRouter();
 const homeStore = useHomeStore();
@@ -258,9 +204,7 @@ const getAmountWallet = async () => {
     const dataAmountWallet: Item[] = res.data;
 
     const localStorageUserId = localStorage.getItem("user_id");
-    matchedData.value = dataAmountWallet.find(
-      (item) => item.user_id === localStorageUserId
-    );
+    matchedData.value = dataAmountWallet.find((item) => item.user_id === localStorageUserId);
   } catch (error) {
     console.error("Error:", error);
   }
