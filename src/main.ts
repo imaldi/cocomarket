d98f68286ffee83a0bfe8e4cc5e6dda8ea98afaf
vijayandika <vijayandika458@gmail.com>
2024-03-09 12:13:53 +0700

    comment login, register

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 import { initVeeValidate } from "./plugins/vee-validate";
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import ElementPlus from "element-plus";
+
+import "element-plus/dist/index.css";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
 import "virtual:uno.css";
@@ -12,9 +13,9 @@ import "./assets/style/style.scss";
 import { Icon } from "@iconify/vue";
 
 const app = createApp(App);
+
 app.use(ElementPlus);
 app.use(createPinia());
 app.use(initVeeValidate);
 app.component("icon", Icon);
 app.use(router).mount("#app");
-
