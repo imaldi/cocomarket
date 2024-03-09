6212511919fa7fed2b230eba48d995f7d0121744
vijayandika <vijayandika458@gmail.com>
2024-03-09 15:06:42 +0700

    sticky bottom

--- src/components/DetailCategoryForm.vue
+++ src/components/DetailCategoryForm.vue
@@ -62,8 +62,8 @@
         </div>
       </div>
 
-      <div v-if="totalItem" @click="GoDetailCart(totalItem.carts_id)" class="relative">
-        <div class="fixed w-full bg-white rounded-lg shadow-md" style="bottom: 0.5em">
+      <div v-if="totalItem && totalItem.total !== 0" @click="GoDetailCart(totalItem.carts_id)" class="relative">
+        <div class="fixed w-full bg-white rounded-lg shadow-md" style="bottom: 0">
           <div class="flex w-full justify-between p-4">
             <div class="flex p-4 mr-8 rounded-2xl bg-primary w-full justify-center text-white">
               <div class="flex justify-between w-full">
