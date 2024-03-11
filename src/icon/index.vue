92569d0a6bd9e71fb53e8d08c79a30b23d01d574
vijayandika <vijayandika458@gmail.com>
2024-03-11 18:29:00 +0700

    checkout

--- src/icon/index.vue
+++ src/icon/index.vue
@@ -6,6 +6,9 @@
     <div v-else-if="icon == 'money'">
       <img :src="money" :width="width" :height="height" alt="money" />
     </div>
+    <div v-else-if="icon == 'money-checkout'">
+      <img :src="moneyCheckout" :width="width" :height="height" alt="moneyCheckout" />
+    </div>
     <div v-else-if="icon == 'withdraw'">
       <img :src="withdraw" :width="width" :height="height" alt="withdraw" />
     </div>
@@ -426,6 +429,7 @@
 <script>
 import wallet from "../assets/icon/wallet.svg";
 import money from "../assets/icon/money.svg";
+import moneyCheckout from "../assets/icon/money-checkout.svg";
 import withdraw from "../assets/icon/withdraw.svg";
 import outlinePlus from "../assets/icon/outline-plus.svg";
 import more from "../assets/icon/more.svg";
@@ -575,6 +579,7 @@ export default {
     return {
       wallet,
       money,
+      moneyCheckout,
       withdraw,
       outlinePlus,
       more,
