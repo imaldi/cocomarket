70bb1940795e689810e430a5367a523368161798
Joe Phang Rahmansyah <joe.phang@icloud.com>
2024-03-16 14:54:44 +0700

    Adjustment Routing and seperating userLocation and Courier Location

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -154,7 +154,7 @@ interface Item {
 
 
 const tracking = async (id: any) => {
-  router.push(`driver/deliverydetails/${id.id}`);
+  router.push(`trackOrder/${id.id}`);
 };
 
 const calculateTotalPrice = (item: any, data: any) => {
